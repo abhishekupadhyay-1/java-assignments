17b1d2e5267bb6931fe723c2aca468e599885542
abhishekupadhyay-1 <jmfabhishek@gmail.com>
2026-08-03 09:58:49 +0530

    folder structure

--- src/triangle.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class triangle {
-public void area(int base, int height){
-    System.out.println("area" + (0.5*base* height));
-}
-public void perimeter(int s1, int s2, int s3){
-    
-}
-}
