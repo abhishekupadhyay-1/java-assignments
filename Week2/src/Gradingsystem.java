33f5b5e63ebc41b211ca1651535b9faaa47dd040
abhishekupadhyay-1 <jmfabhishek@gmail.com>
2026-08-12 09:30:58 +0530

    commit

---
+++ Week2/src/Gradingsystem.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+    public class Gradingsystem {
+        public static void main(String[] args) {
+            Scanner x = new Scanner(System.in);
+             System.out.println("Enter marks:");
+             int marks =x.nextInt();
+
+             if((marks>=80) &&(marks <=100)){
+
+                System.out.println("Distinction");
+             }else if((marks>=70) && (marks<= 79)){
+
+                System.out.println("Merit");
+             }else if((marks>=50) && (marks<=69)){
+
+                System.out.println("Pass");
+             }else{
+
+                System.out.println("Fail");
+             }
+        }}
---
+++ Week2/src/LeapYear.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class LeapYear {
+    public static void main(String[] args){
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter year:");
+        int year = x.nextInt();
+        if((year%400==0) ||((year%4== 0)&&(year%100!= 0))){
+            System.out.println("leap year");
+        }
+        else{
+            System.out.println("not a leap year");
+        }
+}
+}
