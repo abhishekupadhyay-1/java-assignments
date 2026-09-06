57db6ced1ea81cbf50904837d19ea14a2d4ca295
abhishekupadhyay-1 <jmfabhishek@gmail.com>
2026-09-06 14:13:40 +0530

    ShapeManagementApp

--- Week5/src/App.java
+++ Week5/src/App.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        
+       Scanner in = new Scanner(System.in);
+
+       ShapeManagementApp SMA = new ShapeManagementApp();
+       SMA.ShapeManagementApp_main();
+
+       in.close();
     }
 }
